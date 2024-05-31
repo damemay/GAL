caa26659fbc0353868fb658d24544aa429f85685
mar <mar@march>
2024-05-31 14:30:26 +0200

    cleanup and reorganize external dependencies

---
+++ ext/tinygltf.cc
@@ -0,0 +1,7 @@
+#include <stb_image.h>
+#include <stb_image_write.h>
+#define TINYGLTF_IMPLEMENTATION
+#define TINYGLTF_NO_INCLUDE_STB_IMAGE
+#define TINYGLTF_NO_INCLUDE_STB_IMAGE_WRITE
+#define TINYGLTF_USE_CPP14
+#include <tiny_gltf.h>
